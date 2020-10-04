4379e20a4dd572191fe971b91026099df02b85a8
rakeshpawar05 <46896026+rakeshpawar05@users.noreply.github.com>
2020-10-04 14:47:54 +0530

    added react routes

--- realme3pro/src/App.js
+++ realme3pro/src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import Navigation from './components/Navigation/Navigation.js';
 import Introduction from './components/Introduction/Introduction.js';
 import CustomROM from './components/CustomROM/CustomROM.js'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <Introduction />
-      <CustomROM />
-    </div>
+    <Router>
+      <div className="App">
+        <Navigation />
+        <Switch>
+          <Route path='/' exact component={Introduction} />
+          <Route path='/customrom' component={CustomROM} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- realme3pro/src/components/Navigation/Navigation.js
+++ realme3pro/src/components/Navigation/Navigation.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './Navigation.css';
+import {Link} from 'react-router-dom';
 
 const Navigation = () => {
     return (
-        <nav  >   
-            <h3 className = 'icons '>Welcome</h3>
-            <p className = 'icons pointer'>Introduction</p>
-            <p className = 'icons pointer'>Custom ROMs</p>
-            <p className = 'icons pointer'>Stock ROM</p>
-            <p className = 'icons pointer'>Others</p>
+        <nav  >
+            <h3>Welcome</h3>
+            <ul className= 'navitems'>
+                <Link to='/'>
+                    <li>Introduction</li>
+                </Link>
+                <Link to='/customrom'>
+                    <li>Custom ROMs</li>
+                </Link>
+                <Link to='/introduction'>
+                    <li>Stock ROM</li>
+                </Link>
+                <Link to='/introduction'>
+                    <li>Others</li>
+                </Link>
+            </ul>
+
+
         </nav> 
     );
 }
